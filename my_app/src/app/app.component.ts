4edd5a5f5cb9432a8ed03d583a609292d9deb163
Lucas Camargo <lb.camargo94@gmail.com>
2022-11-09 19:47:57 -0300

    ✨ feat(): seção 3.12 - ngOnInit

--- my_app/src/app/app.component.ts
+++ my_app/src/app/app.component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    {{ valor }}
+    <button>Adivionar</button>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {}
+export class AppComponent
+  implements OnInit, DoCheck, AfterContentInit, AfterContentChecked
+{
+  public valor: number = 1;
+  constructor() {}
+
+  public adicionar(): number {
+    return this.valor;
+  }
+
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+}
